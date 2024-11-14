d2983763fb2822dab7f86d279b2c4715bed44a2b
kurrrru <kurrrru8850@gmail.com>
2024-11-15 01:52:57 +0900

    軽微な修正

--- src/lexer/lexer_util.c
+++ src/lexer/lexer_util.c
@@ -6,7 +6,7 @@
 /*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 22:43:13 by nkawaguc          #+#    #+#             */
-/*   Updated: 2024/11/14 23:46:10 by nkawaguc         ###   ########.fr       */
+/*   Updated: 2024/11/15 01:45:40 by nkawaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	flip_quote(const char quote, t_lexer_flag *flag)
 			flag->sep = 0;
 			token_num_diff = 1;
 		}
-		(flag->dquote) = !(flag->dquote);
+		flag->dquote = !flag->dquote;
 	}
 	else if (quote == '\'' && !flag->dquote)
 	{
@@ -48,7 +48,7 @@ int	flip_quote(const char quote, t_lexer_flag *flag)
 			flag->sep = 0;
 			token_num_diff = 1;
 		}
-		(flag->squote) = !(flag->squote);
+		flag->squote = !flag->squote;
 	}
 	return (token_num_diff);
 }
