6eef7f4612123287174bbe8aedfd6a25d6c77812
kurrrru <kurrrru8850@gmail.com>
2024-11-15 13:30:20 +0900

    configを追加

---
+++ src/config/init_config.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_config.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 13:12:48 by nkawaguc          #+#    #+#             */
+/*   Updated: 2024/11/15 13:28:47 by nkawaguc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/config.h"
+
+int	init_config(t_config *config, char **envp)
+{
+	int		i;
+	char	*equal;
+
+	config->exit_status = 0;
+	i = 0;
+	while (envp[i])
+		i++;
+	config->envp = (t_env *)malloc(sizeof(t_env) * (i + 1));
+	if (config->envp == NULL)
+		return (EXIT_FAILURE);
+	config->envp_capacity = i + 1;
+	config->envp_num = i;
+	i = 0;
+	while (envp[i])
+	{
+		equal = ft_strchr(envp[i], '=');
+		if (equal == NULL)
+			continue ;
+		config->envp[i].key = ft_substr(envp[i], 0, equal - envp[i]);
+		config->envp[i].value = ft_strdup(equal + 1);
+		if (config->envp[i].key == NULL || config->envp[i].value == NULL)
+			return (free_config(config), EXIT_FAILURE);
+		i++;
+	}
+}
