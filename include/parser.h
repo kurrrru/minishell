e9ee75c5c25bb78e53fae9c784df95db8bec78bc
kurrrru <kurrrru8850@gmail.com>
2024-11-16 21:29:41 +0900

    parserをnorm対応させた

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 14:18:14 by nkawaguc          #+#    #+#             */
-/*   Updated: 2024/11/16 16:45:58 by nkawaguc         ###   ########.fr       */
+/*   Updated: 2024/11/16 18:51:36 by nkawaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,8 @@ t_node	*parse_paren_left(t_data *data, t_parse_helper *ps, t_config *config,
 t_node	*parse_paren_right(t_data *data, t_parse_helper *ps, t_config *config,
 			t_node *root);
 t_node	*parse_redirect(t_data *data, t_parse_helper *ps, t_config *config,
+			t_node **root);
+t_node	*parse_command(t_data *data, t_parse_helper *ps, t_config *config,
 			t_node **root);
 void	dump_node(t_node *node, int depth);
 void	dump_tree(t_node *root);
--- src/parser/parse_data.c
+++ src/parser/parse_data.c
@@ -6,102 +6,52 @@
 /*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 14:21:17 by nkawaguc          #+#    #+#             */
-/*   Updated: 2024/11/16 17:23:47 by nkawaguc         ###   ########.fr       */
+/*   Updated: 2024/11/16 21:27:40 by nkawaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
+static int	is_logical(t_token token);
 static int	is_redirect(t_token token);
 
-t_node *parse_data(t_data *data, t_parse_helper *ps, t_config *config, const int depth)
+t_node	*parse_data(t_data *data, t_parse_helper *ps,
+	t_config *config, const int depth)
 {
 	t_node	*root;
+	t_token	token;
 
 	root = NULL;
 	while (ps->index < data->token_num)
 	{
-		t_token token = data->token_arr[ps->index];
-		if (token.type == PIPE || token.type == LOGICAL_AND || token.type == LOGICAL_OR)
-		{
-			if ((token.type == LOGICAL_AND || token.type == LOGICAL_OR) && depth > 0)
-				return (ps->index--, root);
-			ps->node = root;
+		token = data->token_arr[ps->index];
+		if (is_logical(token) && depth > 0)
+			return (ps->index--, root);
+		ps->node = root;
+		if (token.type == PIPE || is_logical(token))
 			root = parse_connector(data, ps, config, depth);
-			if (root == NULL)
-				return (NULL);
-		}
 		else if (token.type == PAREN_LEFT)
-		{
-			ps->node = root;
 			root = parse_paren_left(data, ps, config, depth);
-			if (root == NULL)
-				return (NULL);
-		}
 		else if (token.type == PAREN_RIGHT)
 			return (parse_paren_right(data, ps, config, root));
 		else if (is_redirect(token))
-		{
-			if (parse_redirect(data, ps, config, &root) == NULL)
-				return (NULL);
-		}
+			root = parse_redirect(data, ps, config, &root);
 		else if (token.type == WORD)
-		{
-			if (ps->index > 0 && data->token_arr[ps->index - 1].type == PAREN_RIGHT)
-			{
-				ft_putendl_fd("Syntax error: unexpected word after `)'", STDERR_FILENO);
-				config->exit_status = EXIT_INVALID_INPUT;
-				free_tree(root);
-				return (NULL);
-			}
-			if (root == NULL)
-			{
-				root = new_command_node(token);
-				if (root == NULL)
-				{
-					config->exit_status = EXIT_FAILURE;
-					return (NULL);
-				}
-			}
-			else
-			{
-				if (root->command == NULL)
-				{
-					root->command = ft_strdup(token.token);
-					if (root->command == NULL)
-					{
-						perror("malloc");
-						config->exit_status = EXIT_FAILURE;
-						free_tree(root);
-						return (NULL);
-					}
-				}
-				else
-				{
-					if (root->arg_num >= root->arg_capacity)
-					{
-						root->arg_capacity *= 2;
-						root->argv = ft_realloc(root->argv,
-							sizeof(char *) * root->arg_capacity / 2, sizeof(char *) * root->arg_capacity);
-					}
-					root->argv[root->arg_num++] = ft_strdup(token.token);
-					if (root->argv[root->arg_num - 1] == NULL)
-					{
-						perror("malloc");
-						config->exit_status = EXIT_FAILURE;
-						free_tree(root);
-						return (NULL);
-					}
-				}
-			}
-		}
+			root = parse_command(data, ps, config, &root);
+		if (root == NULL)
+			return (NULL);
 		ps->index++;
 	}
-	return root;
+	return (root);
 }
 
 static int	is_redirect(t_token token)
 {
-	return (token.type == REDIRECT_IN || token.type == REDIRECT_OUT ||
-			token.type == REDIRECT_APPEND || token.type == REDIRECT_HEREDOC);
+	return (token.type == REDIRECT_IN || token.type == REDIRECT_OUT
+		|| token.type == REDIRECT_APPEND || token.type == REDIRECT_HEREDOC);
+}
+
+static int	is_logical(t_token token)
+{
+	return (token.type == LOGICAL_AND || token.type == LOGICAL_OR);
 }
