1e93e52e4dd6af46938a2dbea29a39438b8a80ac
kurrrru <kurrrru8850@gmail.com>
2024-11-26 21:54:39 +0900

    不要なコメント等を削除

--- src/parser/parse_heredoc.c
+++ src/parser/parse_heredoc.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/17 22:54:55 by nkawaguc          #+#    #+#             */
-/*   Updated: 2024/11/26 21:51:50 by nkawaguc         ###   ########.fr       */
+/*   Created: 2024/11/26 21:53:27 by nkawaguc          #+#    #+#             */
+/*   Updated: 2024/11/26 21:53:43 by nkawaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,9 @@ static int heredoc_read(t_redirect *redirect, int heredoc_fd[2], t_config *confi
 int	parse_heredoc(t_redirect *redirect, t_config *config)
 {
 	int		heredoc_fd[2];
-	// char	*file;
 
-	// file = redirect->file;
-	// redirect->file = del_quote(redirect->file);
-	// free(file);
-	// if (!redirect->file)
-	// 	return (EXIT_FAILURE);
 	if (pipe(heredoc_fd) == -1)
 		return (perror("pipe"), EXIT_FAILURE);
-	// set heredoc sighandler ?
 	if (heredoc_read(redirect, heredoc_fd, config))
 		return (close(heredoc_fd[0]), close(heredoc_fd[1]), EXIT_FAILURE);
 	close(heredoc_fd[1]);
@@ -36,23 +29,23 @@ int	parse_heredoc(t_redirect *redirect, t_config *config)
 
 static int heredoc_read(t_redirect *redirect, int heredoc_fd[2], t_config *config)
 {
-    pid_t pid;
+	pid_t pid;
 
-    set_heredoc_handler();
-    close(heredoc_fd[0]);
-    pid = fork();
-    if (pid == -1)
-        return (perror("fork"), EXIT_FAILURE);
-    if (pid == 0)
-    {
-        char *line;
+	set_heredoc_handler();
+	close(heredoc_fd[0]);
+	pid = fork();
+	if (pid == -1)
+		return (perror("fork"), EXIT_FAILURE);
+	if (pid == 0)
+	{
+		char *line;
 		char	*expanded;
 		set_heredoc_child_handler();
 		del_quote(redirect->file);
-        while (1)
-        {
-            line = readline("> ");
-            if (!line)
+		while (1)
+		{
+			line = readline("> ");
+			if (!line)
 			{
 				ft_putstr_fd("bash: warning: here-document \
 delimited by end-of-file (wanted `", STDERR_FILENO);
@@ -67,38 +60,14 @@ delimited by end-of-file (wanted `", STDERR_FILENO);
 				return (free(line), EXIT_FAILURE);
 			ft_putendl_fd(expanded, heredoc_fd[1]);
 			free(expanded);
-        }
-        exit(EXIT_SUCCESS);
-    }
-    else
-    {
-        close(heredoc_fd[1]);
-        waitpid(pid, &config->exit_status, 0);
-        config->exit_status = extract_status(config->exit_status);
-    }
-    return EXIT_SUCCESS;
+		}
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		close(heredoc_fd[1]);
+		waitpid(pid, &config->exit_status, 0);
+		config->exit_status = extract_status(config->exit_status);
+	}
+	return EXIT_SUCCESS;
 }
-
-// static int	heredoc_read(t_redirect *redirect, int heredoc_fd[2])
-// {
-// 	char	*line;
-
-// 	while (1)
-// 	{
-// 		set_heredoc_handler();
-// 		line = readline("> ");
-// 		if (!line)
-// 		{
-// 			ft_putstr_fd("bash: warning: here-document 
-// delimited by end-of-file (wanted `", STDERR_FILENO);
-// 			ft_putstr_fd(redirect->file, STDERR_FILENO);
-// 			ft_putendl_fd("')", STDERR_FILENO);
-// 			return (EXIT_SUCCESS);
-// 		}
-// 		if (ft_strcmp(line, redirect->file) == 0)
-// 			return (free(line), EXIT_SUCCESS);
-// 		ft_putendl_fd(line, heredoc_fd[1]);
-// 		free(line);
-// 	}
-// 	return (EXIT_SUCCESS);
-// }
