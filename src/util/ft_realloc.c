16b422484d7cda9981e5f96994e8891a70d7e8bf
kurrrru <kurrrru8850@gmail.com>
2024-11-26 23:15:38 +0900

    ft_reallocをupdateして、昔のデータがない部分は0となるようにした

--- src/util/ft_realloc.c
+++ src/util/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 22:45:51 by nkawaguc          #+#    #+#             */
-/*   Updated: 2024/11/14 23:08:53 by nkawaguc         ###   ########.fr       */
+/*   Updated: 2024/11/26 23:14:34 by nkawaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
+	ft_bzero(new_ptr, new_size);
 	ft_memcpy(new_ptr, ptr, old_size);
 	free(ptr);
 	return (new_ptr);
